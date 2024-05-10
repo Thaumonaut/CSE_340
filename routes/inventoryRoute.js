b37f40cb50f2db112e7499cf7b8e3fb8c6478b62
jacobperry <perryjacob@outlook.com>
2024-05-10 21:38:29 +0000

    Wrapped Inventory routes in error handlers

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,7 +3,7 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
 
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.serverError(invController.buildByClassificationId));
 router.get("/detail/:vehicleId", utilities.serverError(invController.buildByVehicleId));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require('./routes/inventoryRoute')
 const utilities = require('./utilities/')
-const { populate } = require("dotenv")
 
 /* ***********************
  * View Engine and Templates
